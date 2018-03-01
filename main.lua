4645e2eca6275c5f73b2fc691b12c47f39ae68b2
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-03-01 10:46:06 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+-- Title: MovingImages
+-- Name: Your Name
+-- Course: ICS2O/3C
+-- This program displays a beetleship moving across the screen as well as another
+--object moving in a different direction.
+
+print ("***Testing beetleship!")
+
+-- hide the status bar
+display.setstatusBar
